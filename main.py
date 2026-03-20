7c396377c8bb117ebef4f0a68fb5fa4892370f24
Steven <steven.nguyen561318@gmail.com>
2026-03-20 21:53:55 +0100

    Init Commit (App et fonction de base)

---
+++ main.py
@@ -0,0 +1,52 @@
+import sys
+import os
+
+def exit():
+    sys.exit(0)
+
+def ls():
+    path = os.getcwd()
+    fichiers = os.listdir(path)
+    for fichier in fichiers:
+        print(fichier)
+
+def createFile():
+    fichier = input("Entrer le nom du fichier : ")
+    f = open(fichier,'w')
+    f.close()
+
+def createFolder():
+    folder = input("Entrer le nom du dossier : ")
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+def cd(option):
+    if option == "..":
+        pass
+
+def Terminal():
+    commandList = ["BJ", "exit", "ls", "createFile"]
+    while True:
+        command = input(">>> ")
+
+        # Test
+        if command == "BJ":
+            sys.stdout.write("Gambler de merde")
+            sys.exit(0)
+
+        #Affichage de répertoire
+        if command == "ls":
+            ls()
+
+        #Création d'un fichier
+        if command == "createFile":
+            createFile()
+
+        # Commande de sortie
+        if command == "exit":
+            exit()
+        elif command not in commandList:
+            print(f"{command} : Commande inconnue")
+
+if __name__ == '__main__':
+    Terminal()
